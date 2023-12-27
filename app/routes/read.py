856ae8c3c7b831dbe2915ef7d179099893f6fe81
Vincent Carrier <vincentcarrier15@gmail.com>
2023-12-27 12:25:50 -0500

    DRY up author corpuses

--- app/routes/read.py
+++ app/routes/read.py
@@ -4,6 +4,7 @@ from os import listdir
 from typing import NamedTuple
 from box import Box
 from flask import Blueprint, render_template, request
+from slugify import slugify
 from werkzeug.exceptions import NotFound
 
 from core.constants import CHUNKS, DATA
@@ -16,34 +17,41 @@ from core.corpus import corpus
 bp = Blueprint("read", __name__, url_prefix="/")
 
 
-class Doc(NamedTuple):
-    slug: str
-    subdocs: list[str]
-
-
 json_str = (CHUNKS / "index.json").read_text()
 index = Box(JSONDecoder().decode(json_str))
 
-homer = itemgetter("iliad", "odyssey")(index)
-for doc in homer:
-    doc.get_book = lambda span: Ref.parse(span).start.chapter
 
-aeschylus = itemgetter("agamemnon", "libationbearers", "eumenides")(index)
+class AuthorCorpus(NamedTuple):
+    name: str
+    slug: str
+    corpus: Box
+
+
+def author_corpus(author: str) -> AuthorCorpus:
+    corpus = {slug: meta for slug, meta in index.items() if meta.author == author}
+    return AuthorCorpus(author, slugify(author), Box(corpus))
+
+
+homer = author_corpus("Homer")
+for doc in homer.corpus.values():
+    doc.get_book = lambda span: Ref.parse(span).start.chapter
+    doc.get_chunk_title = lambda span: f"Book {doc.get_book(span)}"
+
+aeschylus = author_corpus("Aeschylus")
+
+authors = [homer, aeschylus]
 
 
 @bp.route("/")
 def get_index():
-    return render_template("index.html", homer=homer, aeschylus=aeschylus)
+    return render_template("index.html", authors=authors)
 
 
 def render_reader(tb: Treebank, slug: str, span: str | None = None):
     body = HtmlPartialRenderer(tb).render()
     meta = Box(index[slug])
-    subdoc = None
-    if span:
-        ref = Ref.parse(span)
-        subdoc = f"Book {ref.chapter}"  # type: ignore TODO
-    return render_template("reader.html", body=body, subdoc=subdoc, **meta)
+    chunk_title = meta.get_chunk_title(span) if span else None  # type: ignore
+    return render_template("reader.html", body=body, chunk_title=chunk_title, **meta)
 
 
 @bp.route("/read/<slug>")
@@ -61,4 +69,4 @@ def get_chunk(slug: str, span: str):
     if not f.exists():
         raise NotFound(f"Unknown document {slug}/{span}")
     tb = PerseusTB(f, None, None)  # type: ignore
-    return render_reader(tb, slug)
+    return render_reader(tb, slug, span)
